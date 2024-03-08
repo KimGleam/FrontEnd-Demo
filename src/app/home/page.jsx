c5ff88a3891d1ec5f8522afb487bbac504e4354e
김태욱(메가존) <kimtw@mz.co.kr>
2024-03-08 16:45:12 +0900

    Main 페이지 수정

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -11,6 +11,7 @@ import {
     Menu,
     MenuItem, ListItemText, Select, InputLabel, FormControl,
 } from '@mui/material';
+import * as React from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
@@ -20,6 +21,16 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useState } from 'react';
 import {MenuList} from "@mui/joy";
 import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase'
+import Badge from '@mui/material/Badge';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+
 
 const searchCategories = [
     { id: 1, name: 'Category 1' },
@@ -130,8 +141,143 @@ const Home = () => {
     const [currentPromotionIndex, setCurrentPromotionIndex] = useState(0);
     const [currentEventIndex, setCurrentEventIndex] = useState(0);
     const [currentDiscountIndex, setCurrentDiscountIndex] = useState(0);
-    const [anchorEl, setAnchorEl] = useState(null);
-    const [selectedCategory, setSelectedCategory] = useState('');
+
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    }));
+
+    const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }));
+
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        '& .MuiInputBase-input': {
+            padding: theme.spacing(1, 1, 1, 0),
+            // vertical padding + font size from searchIcon
+            paddingLeft: `calc(1em + ${theme.spacing(3)})`,
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('md')]: {
+                width: '20ch',
+            },
+        },
+    }));
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    };
+
+    const handleMobileMenuOpen = (event) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+    const menuId = 'primary-search-account-menu';
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+            }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        </Menu>
+    );
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = (
+        <Menu
+            anchorEl={mobileMoreAnchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'center',
+                horizontal: 'center',
+            }}
+            open={isMobileMenuOpen}
+            onClose={handleMobileMenuClose}
+        >
+            <MenuItem>
+                <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                    <Badge badgeContent={4} color="error">
+                        <MailIcon />
+                    </Badge>
+                </IconButton>
+                <p>Messages</p>
+            </MenuItem>
+            <MenuItem>
+                <IconButton
+                    size="large"
+                    aria-label="show 17 new notifications"
+                    color="inherit"
+                >
+                    <Badge badgeContent={17} color="error">
+                        <NotificationsIcon />
+                    </Badge>
+                </IconButton>
+                <p>Notifications</p>
+            </MenuItem>
+            <MenuItem onClick={handleProfileMenuOpen}>
+                <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="primary-search-account-menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle />
+                </IconButton>
+                <p>Profile</p>
+            </MenuItem>
+        </Menu>
+    );
 
     const handlePrevProduct = () => {
         setCurrentProductIndex(prevIndex => (prevIndex === 0 ? products.length - 1 : prevIndex - 1));
@@ -165,89 +311,84 @@ const Home = () => {
         setCurrentDiscountIndex(prevIndex => (prevIndex === discounts.length - 1 ? 0 : prevIndex + 1));
     };
 
-    const handleCategoryButtonClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleCategoryClose = () => {
-        setAnchorEl(null);
-    };
-
-    const handleCategoryChange = (event) => {
-        setSelectedCategory(event.target.value);
-    };
-
     return (
+        <body style={{margin:0}}>
         <Box>
-            {/* 헤더 */}
-            <Container sx={{borderRadius: 10,position: 'fixed', left: '50%', transform: 'translate(-50%, 0)', backgroundColor:'white', paddingTop:1, paddingBottom:1, paddingLeft: 100, paddingRight: 100}}>
-                <Grid container justifyContent="space-between" alignItems="center" sx={{ py: 0 }}>
-                    <Grid item>
-                        <IconButton color="inherit" sx={{ mr: 2 }} onClick={handleCategoryButtonClick}>
+            {/*헤더 영역*/}
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="fixed">
+                    <Toolbar sx={{marginLeft:30, marginRight:30}}>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="open drawer"
+                            sx={{ mr: 2 }}
+                        >
                             <MenuIcon />
                         </IconButton>
-                        <FormControl variant="outlined" sx={{marginLeft: 10}}>
-                            <InputLabel id="category-label">Category</InputLabel>
-                            <Select
-                                labelId="category-label"
-                                id="category"
-                                value={selectedCategory}
-                                onChange={handleCategoryChange}
-                                label="Category"
-                                sx={{width:200}}
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="div"
+                            sx={{ display: { xs: 'none', sm: 'block' } }}
+                        >
+                            MUI
+                        </Typography>
+                        <Search sx={{}}>
+                            <SearchIconWrapper>
+                                <SearchIcon />
+                            </SearchIconWrapper>
+                            <StyledInputBase
+                                placeholder="Search…"
+                                inputProps={{ 'aria-label': 'search' }}
+                            />
+                        </Search>
+                        <Box sx={{ flexGrow: 1 }} />
+                        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                                <Badge badgeContent={4} color="error">
+                                    <MailIcon />
+                                </Badge>
+                            </IconButton>
+                            <IconButton
+                                size="large"
+                                aria-label="show 17 new notifications"
+                                color="inherit"
                             >
-                                {searchCategories.map((category) => (
-                                    <MenuItem key={category.id} value={category.id}>
-                                        {category.name}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                        <TextField id="search" label="Search" variant="outlined" sx={{ ml: 1, width: 600 }} />
-                        <IconButton color="inherit">
-                            <SearchIcon sx={{width: 40, height: 40}} />
-                        </IconButton>
-                    </Grid>
-                    <Grid item>
-                        <IconButton color="inherit">
-                            <ShoppingCartIcon sx={{width: 40, height: 40}} />
-                        </IconButton>
-                        <IconButton color="inherit">
-                            <AccountCircleIcon sx={{width: 40, height: 40}} />
-                        </IconButton>
-                    </Grid>
-                </Grid>
-
-                {/* 카테고리 메뉴 */}
-                <Popover
-                    open={Boolean(anchorEl)}
-                    anchorEl={anchorEl}
-                    onClose={handleCategoryClose}
-                    anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                    }}
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'left',
-                    }}
-                >
-                    <MenuList autoFocusItem={Boolean(anchorEl)} id="menu-list-grow" onKeyDown={handleCategoryClose} sx={{width: 170, height: 300}}>
-                        {categories.map((category) => (
-                            <MenuItem
-                                key={category.id}
+                                <Badge badgeContent={17} color="error">
+                                    <NotificationsIcon />
+                                </Badge>
+                            </IconButton>
+                            <IconButton
+                                size="large"
+                                edge="end"
+                                aria-label="account of current user"
+                                aria-controls={menuId}
+                                aria-haspopup="true"
+                                onClick={handleProfileMenuOpen}
+                                color="inherit"
                             >
-                                <ListItemText primary={category.name} />
-                                {category.children.length > 0 && (
-                                    <IconButton size="small">
-                                        <ArrowForwardIcon />
-                                    </IconButton>
-                                )}
-                            </MenuItem>
-                        ))}
-                    </MenuList>
-                </Popover>
-            </Container>
+                                <AccountCircle />
+                            </IconButton>
+                        </Box>
+                        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="show more"
+                                aria-controls={mobileMenuId}
+                                aria-haspopup="true"
+                                onClick={handleMobileMenuOpen}
+                                color="inherit"
+                            >
+                                <MoreIcon />
+                            </IconButton>
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+                {renderMobileMenu}
+                {renderMenu}
+            </Box>
 
             {/* 이벤트 영역 */}
             <Box sx={{ p: 2, mb: 2, paddingTop: 13, height: 400 }}>
@@ -367,6 +508,7 @@ const Home = () => {
                 </Grid>
             </Box>
         </Box>
+        </body>
     );
 };
 
