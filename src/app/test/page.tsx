3a4e86932a159f5fd6948c6000a0393ece5f7964
김태욱(메가존) <kimtw@mz.co.kr>
2024-03-11 17:49:39 +0900

    Carousel 테스트

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -11,86 +11,179 @@ import Chip from '@mui/joy/Chip';
 import Link from '@mui/joy/Link';
 import Typography from '@mui/joy/Typography';
 import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
-import { CardActions } from '@mui/material';
+import { CardActions, IconButton } from '@mui/material';
+import {BookmarkAdd} from "@mui/icons-material";
 
 
 export default () => {
     const [activeItemIndex, setActiveItemIndex] = useState(0);
     const chevronWidth = 40;
     return (
-        <div style={{ padding: `0 ${chevronWidth}px` }}>
+        <div style={{ padding: `0 ${chevronWidth}px`, width: 1200 }}>
             <ItemsCarousel
                 requestToChangeActive={setActiveItemIndex}
                 activeItemIndex={activeItemIndex}
-                numberOfCards={2}
-                gutter={20}
+                numberOfCards={4}
+                gutter={0}
                 leftChevron={<button>{'<'}</button>}
                 rightChevron={<button>{'>'}</button>}
                 outsideChevron
                 chevronWidth={chevronWidth}
             >
-                <Card sx={{ minWidth: 275 }}>
-                    <CardContent>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            Word of the Day
-                        </Typography>
-                        <Typography variant="h5" component="div">
-                            123
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                            adjective
-                        </Typography>
-                        <Typography variant="body2">
-                            well meaning and kindly.
-                            <br />
-                            {'"a benevolent smile"'}
-                        </Typography>
+                <Card sx={{ width: 250 }}>
+                    <div>
+                        <Typography level="title-lg">Yosemite National Park</Typography>
+                        <Typography level="body-sm">April 24 to May 02, 2021</Typography>
+                    </div>
+                    <AspectRatio minHeight="120px" maxHeight="200px">
+                        <img
+                            src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286"
+                            srcSet="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x"
+                            loading="lazy"
+                            alt=""
+                        />
+                    </AspectRatio>
+                    <CardContent orientation="horizontal">
+                        <div>
+                            <Typography level="body-xs">Total price:</Typography>
+                            <Typography fontSize="lg" fontWeight="lg">
+                                $2,900
+                            </Typography>
+                        </div>
+                        <Button
+                            variant="solid"
+                            size="md"
+                            color="primary"
+                            aria-label="Explore Bahamas Islands"
+                            sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
+                        >
+                            Explore
+                        </Button>
                     </CardContent>
-                    <CardActions>
-                        <Button size="small">Learn More</Button>
-                    </CardActions>
                 </Card>
-                <Card sx={{ minWidth: 275 }}>
-                    <CardContent>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            Word of the Day
-                        </Typography>
-                        <Typography variant="h5" component="div">
-                            123
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                            adjective
-                        </Typography>
-                        <Typography variant="body2">
-                            well meaning and kindly.
-                            <br />
-                            {'"a benevolent smile"'}
-                        </Typography>
+                <Card sx={{ width: 250 }}>
+                    <div>
+                        <Typography level="title-lg">Yosemite National Park</Typography>
+                        <Typography level="body-sm">April 24 to May 02, 2021</Typography>
+                    </div>
+                    <AspectRatio minHeight="120px" maxHeight="200px">
+                        <img
+                            src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286"
+                            srcSet="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x"
+                            loading="lazy"
+                            alt=""
+                        />
+                    </AspectRatio>
+                    <CardContent orientation="horizontal">
+                        <div>
+                            <Typography level="body-xs">Total price:</Typography>
+                            <Typography fontSize="lg" fontWeight="lg">
+                                $2,900
+                            </Typography>
+                        </div>
+                        <Button
+                            variant="solid"
+                            size="md"
+                            color="primary"
+                            aria-label="Explore Bahamas Islands"
+                            sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
+                        >
+                            Explore
+                        </Button>
                     </CardContent>
-                    <CardActions>
-                        <Button size="small">Learn More</Button>
-                    </CardActions>
                 </Card>
-                <Card sx={{ minWidth: 275 }}>
-                    <CardContent>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            Word of the Day
-                        </Typography>
-                        <Typography variant="h5" component="div">
-                            123
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                            adjective
-                        </Typography>
-                        <Typography variant="body2">
-                            well meaning and kindly.
-                            <br />
-                            {'"a benevolent smile"'}
-                        </Typography>
+                <Card sx={{ width: 250 }}>
+                    <div>
+                        <Typography level="title-lg">Yosemite National Park</Typography>
+                        <Typography level="body-sm">April 24 to May 02, 2021</Typography>
+                    </div>
+                    <AspectRatio minHeight="120px" maxHeight="200px">
+                        <img
+                            src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286"
+                            srcSet="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x"
+                            loading="lazy"
+                            alt=""
+                        />
+                    </AspectRatio>
+                    <CardContent orientation="horizontal">
+                        <div>
+                            <Typography level="body-xs">Total price:</Typography>
+                            <Typography fontSize="lg" fontWeight="lg">
+                                $2,900
+                            </Typography>
+                        </div>
+                        <Button
+                            variant="solid"
+                            size="md"
+                            color="primary"
+                            aria-label="Explore Bahamas Islands"
+                            sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
+                        >
+                            Explore
+                        </Button>
                     </CardContent>
-                    <CardActions>
-                        <Button size="small">Learn More</Button>
-                    </CardActions>
+                </Card>
+                <Card sx={{ width: 250 }}>
+                    <div>
+                        <Typography level="title-lg">Yosemite National Park</Typography>
+                        <Typography level="body-sm">April 24 to May 02, 2021</Typography>
+                    </div>
+                    <AspectRatio minHeight="120px" maxHeight="200px">
+                        <img
+                            src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286"
+                            srcSet="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x"
+                            loading="lazy"
+                            alt=""
+                        />
+                    </AspectRatio>
+                    <CardContent orientation="horizontal">
+                        <div>
+                            <Typography level="body-xs">Total price:</Typography>
+                            <Typography fontSize="lg" fontWeight="lg">
+                                $2,900
+                            </Typography>
+                        </div>
+                        <Button
+                            variant="solid"
+                            size="md"
+                            color="primary"
+                            aria-label="Explore Bahamas Islands"
+                            sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
+                        >
+                            Explore
+                        </Button>
+                    </CardContent>
+                </Card>
+                <Card sx={{ width: 250 }}>
+                    <div>
+                        <Typography level="title-lg">Yosemite National Park</Typography>
+                        <Typography level="body-sm">April 24 to May 02, 2021</Typography>
+                    </div>
+                    <AspectRatio minHeight="120px" maxHeight="200px">
+                        <img
+                            src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286"
+                            srcSet="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x"
+                            loading="lazy"
+                            alt=""
+                        />
+                    </AspectRatio>
+                    <CardContent orientation="horizontal">
+                        <div>
+                            <Typography level="body-xs">Total price:</Typography>
+                            <Typography fontSize="lg" fontWeight="lg">
+                                $2,900
+                            </Typography>
+                        </div>
+                        <Button
+                            variant="solid"
+                            size="md"
+                            color="primary"
+                            aria-label="Explore Bahamas Islands"
+                            sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
+                        >
+                            Explore
+                        </Button>
+                    </CardContent>
                 </Card>
             </ItemsCarousel>
         </div>
